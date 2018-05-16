8815c816227f1cc97a8583280007bfa75c7e4cb4
MikaGrootenboer <0925620@hr.nl>
2018-05-16 15:38:06 +0200

    timestamp function added

---
+++ BBC/BBC/Times.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBC
+{
+    class Times
+    {
+        public String GetTimestamp(DateTime value)
+        {
+            return value.ToString("dd/MM/yyyy>>HH:mm:ss");
+        }
+        public String PrintTimeStamp()
+        {
+            string TimeStamp = GetTimestamp(DateTime.Now);
+            Console.WriteLine(GetTimestamp(DateTime.Now));
+            Console.ReadKey();
+            return TimeStamp;
+        }
+    }
+}
