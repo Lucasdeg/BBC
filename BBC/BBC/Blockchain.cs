fda326fd6b9a1e3a5da730db6b1ce3ee8f55b83d
MikaGrootenboer <0925620@hr.nl>
2018-05-17 11:25:26 +0200

    testing with the bchain

---
+++ BBC/BBC/Blockchain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBC
+{
+    class Blockchain
+    {
+        List<Block> chain = new List<Block>();
+
+        public Blockchain()
+        {
+            this.chain = 
+        }
+
+
+        public  Block Genblock()
+        {
+            return new Block("0", "This is the gen");
+        }
+        public Block GetLast()
+        {
+            return chain[chain.Count-1];
+        }
+        public Block Addblock(Block newblock)
+        {
+            newblock.previousHash = GetLast().blockHash;
+            newblock.blockHash = newblock.CurrentBlockHash();
+            return newblock;
+        }
+
+    }
+}
