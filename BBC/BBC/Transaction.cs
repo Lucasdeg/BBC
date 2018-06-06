e0fe59db79221c80be408442364d61f073fea933
Lucas de Graaf <0932058@hr.nl>
2018-06-06 14:04:48 +0200

    Transaction class in progress

---
+++ BBC/BBC/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBC
+{
+    class Transaction
+    {
+        public string transactionId;
+        public User sender;
+        public User recipient;
+        public string message;
+        public byte[] signature;
+
+        private static int sequence = 0;
+
+        public Transaction(User sender, User recipient, string ipfsHash)
+        {
+            this.sender = sender;
+            this.recipient = recipient;
+            this.message = ipfsHash;
+        }
+
+        public void generateSignature(string privateKey)
+        {
+            string data = null;
+        }
+    }
+}
